b8b70299bbe653ce9fd4699d451fefb0f74731d9
jeongseo21 <jeongseo21@gmail.com>
2020-12-09 17:34:51 +0900

    add update card

--- app.py
+++ app.py
@@ -64,12 +64,15 @@ def logout():
     session.clear()
     return redirect('/')
 
+# 카드 생성
 @app.route('/main', methods=['GET','POST', 'DELETE'])
 def main():
+    # 카드 listing
     if request.method == 'GET':
         cards = list(db.main.find({},{'_id':0}).sort('created_datetime', -1))
         return render_template("main.html", cards=cards)
 
+    # 카드 create
     elif request.method == 'POST':
         card_id_receive = request.form['card_id_give']
         booktitle_receive = request.form['booktitle_give']
@@ -95,29 +98,25 @@ def main():
         }
         db.main.insert_one(data)
         return redirect('/main')
+    # 카드 delete
     else:
         card_id_receive = request.form['card_id_give']
         db.main.delete_one({ "card_id": card_id_receive })
         return render_template('main.html')
     
+# 카드 수정
+@app.route('/main/update', methods=['GET','POST'])
+def update():
+    if request.method == 'GET':
+        pass
+    else:
+        card_id_receive = request.form['target_card_id_give']
+        target_card = db.main.find_one({'card_id':card_id_receive},{'_id':0})
+        
+        return render_template('main.html', target_card = target_card)
+        
 
-
-'''
-# 로그인확인 구현할때 참고하려고 넣어놓은 주석
-
-#로그인을 했는지 안했는지 판단
-routes.py from flask import Flask, render_template, request, make_response 
-from flask import session, redirect, url_for app = Flask(__name__) app.secret_key = 'any random string' # 자바의 welcomlist와 같이 만들어지는 부분 
-
-@app.route('/') 
-def index(): 
-if 'username' in session: 
-# session 내부에 username 이라는 키 값이 없으면 바로 return으로 간다. 
-username = session['username'] # 있으면 해당 키에 대한 값을 꺼내서 
-return 'Logged in as ' + username + '<br>' + \ # 이 부분을 리턴시킨다. 
-'''
-
-
+        
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
